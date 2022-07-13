3093accc8e43e17d1255adfb3704d0fe30612663
Dvir kopolovich <64788488+dvirko@users.noreply.github.com>
2022-07-13 12:24:45 +0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import {useState, createContext, useContext,useEffect} from "react";
 import './App.css';
+import useFetch from "./useFetch";
+const UserContext = createContext()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return(
+    <>
+    <PersonList/>
+    </>
+  );
+}
+const PersonList = () => {
+    const [data] = useFetch('https://node-js-server.dvirko.repl.co/');
+    
+    const [imageData, setImageData] = useState('');
+    useEffect(() =>{
+    fetch('https://node-js-server.dvirko.repl.co/image')
+        .then(response => response.blob())
+        .then(image => {
+            // Create a local URL of that image
+            const localUrl = URL.createObjectURL(image);
+            setImageData(localUrl);
+        });
+}, []);
+  
+  
+    const [person,SetPerson] = useState("");
+
+    return (
+        <UserContext.Provider value={person}>
+        {data &&
+          data.map((person,i) => {
+             return <button key={i} onClick={()=>SetPerson(person)}>{person.name}</button>;
+          })}
+          <img alt='img' src={imageData}/>
+         <PersonData/>
+        </UserContext.Provider>
+    );
+  };
+
+const PersonData = () => {
+  const data = useContext(UserContext);
+  return(
+    <>
+       {Object.keys(data).map((key, index) => {
+        return (
+          <div key={index}>
+              {key}:{data[key]}
+          </div>
+        );
+      })}
+    </>
   );
 }
 
-export default App;
+export default App
